8593715f8e266d8b76e93d3391237d2a3a178cc7
LDX5200 <78689065+LDX5200@users.noreply.github.com>
2023-07-13 21:34:27 +0800

    Add files via upload

---
+++ day2/day2_prac_1.py
@@ -0,0 +1,10 @@
+# print("welcome ! please tell us your age:")
+# age = int(input()) 
+age = int(input("welcome ! please tell us your age:")) 
+# age = input()
+# age = int(age) #注意格式
+if(age >= 18):
+    print("10 yuan more")
+else:
+    print("just come in!")
+print("welcome!")
